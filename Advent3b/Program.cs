68cd9cd9b5efedd735485586c41a48b743259883
John Hutchinson <jhutchinson@scsglobalservices.com>
2024-12-19 14:36:56 -0500

    Add day 3

---
+++ Advent3b/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main()
+    {
+        var total = 0;
+        var filePath = "..\\..\\..\\input.txt";
+        var line = File.ReadAllText(filePath);
+        line = line.Replace("\r\n", "");
+
+        var dontDoOperations = new Regex(@"don't\(\).*?(?:do\(\)|$)");
+        line = dontDoOperations.Replace(line, "");
+
+        var mulOperations = new Regex(@"mul\((?<num1>\d+),(?<num2>\d+)\)");
+        foreach (Match match in mulOperations.Matches(line))
+        {
+            var num1 = int.Parse(match.Groups["num1"].Value);
+            var num2 = int.Parse(match.Groups["num2"].Value);
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            total += num1 * num2;
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
